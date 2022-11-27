04ebae3f49887dafbe7643a21ad2d33408840fec
Donutask <51319000+Donutask@users.noreply.github.com>
2022-11-27 15:10:07 +1100

    added stuff and things

--- script.js
+++ script.js
@@ -1,19 +1,110 @@
-function CreateBubbleWrap() {
-    for (let i = 0; i < 10; i++) {
-        CreateBubble();
+let bubbleLayout = document.getElementById("bubblesLayout");
+let popCounter = document.getElementById("popCount");
 
+
+function CreateBubbleWrap() {
+    bubbleLayout.innerHTML = "";
+
+    let columns = Math.round(((bubbleLayout.clientWidth - 50) / 138));
+    let rows = Math.round((bubbleLayout.clientHeight - 50 - 80) / 138);
+
+    bubbleLayout.style.gridTemplateColumns = `repeat(${columns}, 138px)`;
+    bubbleLayout.style.gridTemplateRows = `repeat(${rows}, 138px)`;
+
+    bubblesCreated = columns * rows;
+
+    for (let i = 0; i < bubblesCreated; i++) {
+        CreateBubble();
     }
+
+    bubblesPopped = 0;
+
+    Save();
 }
 
-let bubbleLayout = document.getElementById("bubblesLayout");
+let bubblesCreated = 0;
+let bubblesPopped = 0;
+let totalBubblesPopped = 0;
 
 function CreateBubble() {
     let bubble = document.createElement("button");
     bubble.className = "bubble";
+    //pop with mouse or touch
+    bubble.onmouseenter = function() { PopBubble(bubble) };
+    bubble.ontouchstart = function() { PopBubble(bubble) };
 
     bubbleLayout.appendChild(bubble);
 
     return bubble;
 }
 
+function PopBubble(bubble) {
+    //don't pop twice
+    if (bubble.classList.contains("poppedBubble")) {
+        return;
+    }
+    bubble.classList.add("poppedBubble");
+    bubblesPopped++;
+
+    totalBubblesPopped++;
+    popCounter.innerHTML = totalBubblesPopped;
+
+    PopSound();
+
+    if (bubblesPopped >= bubblesCreated) {
+        CreateBubbleWrap();
+    }
+}
+
+let audioClips = [
+    new Audio("/Pops/Pop1.wav"),
+    new Audio("/Pops/Pop2.wav"),
+    new Audio("/Pops/Pop3.wav"),
+    new Audio("/Pops/Pop4.wav"),
+    new Audio("/Pops/Pop5.wav"),
+]
+
+function PopSound() {
+    audioClips[getRandomInt(0, 4)].play();
+}
+
+
+function Save() {
+    localStorage.setItem("pops", totalBubblesPopped);
+}
+
+function Load() {
+    //make pop count a positive integer
+    totalBubblesPopped = Number.parseInt(localStorage.getItem("pops"));
+    if (!Number.isInteger(totalBubblesPopped)) {
+        totalBubblesPopped = 0;
+    }
+
+    if (totalBubblesPopped < 0) {
+        totalBubblesPopped = 0;
+    }
+
+    //first time? say what it is
+    if (totalBubblesPopped == 0) {
+        popCounter.innerHTML = "Bubble Wrap";
+    } else {
+        popCounter.innerHTML = totalBubblesPopped;
+    }
+}
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive).
+ * The value is no lower than min (or the next integer greater than min
+ * if min isn't an integer) and no greater than max (or the next integer
+ * lower than max if max isn't an integer).
+ * Using Math.round() will give you a non-uniform distribution!
+ * https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+ */
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+Load();
 CreateBubbleWrap();
